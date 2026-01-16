280eb113f31427055853c84c584186271b0d4c34
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2026-01-15 20:36:45 -0800

    setup frontend and backend

---
+++ backend/src/main/java/com/fluxly/backend/FluxlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fluxly.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxlyBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fluxly/backend/FluxlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxly.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
